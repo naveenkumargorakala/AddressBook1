17a19fa396101a5bb928423673beb9292b36cb59
naveenkumargorakala <nicenaveengorakala@gmail.com>
2023-03-30 17:05:40 +0530

    Add: Edit AddressBookContact

---
+++ AddressBook/AddresBook.java
@@ -0,0 +1,120 @@
+package com.bridgelabz.AddressBook;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+public class AddresBook {
+    public int contactId;
+    List<Person> list1 = new ArrayList<>();
+    Person person = new Person();
+    public void Details() {
+        String FirstName, LastName, Gmail, Address, City, State, Zipcode, PhoneNumber;
+        Random generatingId = new Random();//Using random function to generate contact id
+        Scanner in = new Scanner(System.in);
+        System.out.println("FirstName: ");
+        person.setFirstName(in.next());
+        System.out.println("LastName: ");
+        LastName = in.next();
+        person.setLastName(LastName);
+        System.out.println("Gmail: ");
+        Gmail = in.next();
+        person.setGmail(Gmail);
+        System.out.println("Address: ");
+        Address = in.next();
+        person.setAddress(Address);
+        System.out.println("City: ");
+        City = in.next();
+        person.setCity(City);
+        System.out.println("State: ");
+        State = in.next();
+        person.setState(State);
+        System.out.println("Zipcode: ");
+        Zipcode = in.next();
+        person.setZipcode(Zipcode);
+        System.out.println("Phone: ");
+        PhoneNumber = in.next();
+        person.setPhoneNumber(PhoneNumber);
+        System.out.println("Added Successfully contact");
+
+        list1.add(person);
+    }
+        private int getContactIndex(String name) {
+            for (int i = 0; i < list1.size(); i++) {
+                if (list1.get(i).getFirstName().equals(name)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    public void DisplayContact1(){
+        System.out.println("FirstName: "+person.getFirstName() +"\n LastName: "+person.getLastName() +"\n Gmail: "+person.getGmail()+"\n Address: "+person.getAddress() +"\n City: "+person.getCity() +"\n State: "+person.getState()+"\n ZipCode: "+person.getZipcode()+"\n PhoneNumber: "+person.getPhoneNumber());
+    }
+    public void edit() {
+        String FirstName, LastName, Gmail, Address, City, State, Zipcode, PhoneNumber;
+        Person person =new Person();
+        System.out.println("Enter What you Want to edit:");
+        System.out.println("1:FirstName,2:LastName,3:Gmail,4:Address,5:City,6:State,7:Zipcode,8:Phonenumber");
+        Scanner in = new Scanner(System.in);
+        String label = in.next();
+        switch (label) {
+            case "1":
+                System.out.println("changed first name: ");
+                FirstName = in.next();
+                person.setFirstName(FirstName);
+                break;
+            case "2":
+                System.out.println("lastname to change: ");
+                LastName = in.next();
+                person.setLastName(LastName);
+                break;
+            case "3":
+                System.out.println("gmail to change: ");
+                Gmail = in.next();
+                person.setGmail(Gmail);
+                break;
+            case "4":
+                System.out.println("Address to change: ");
+                Address = in.next();
+                person.setAddress(Address);
+                break;
+            case "5":
+                System.out.println("City to change: ");
+                City = in.next();
+                person.setCity(City);
+                break;
+            case "6":
+                System.out.println("State to change: ");
+                State = in.next();
+                person.setState(State);
+                break;
+            case "7":
+                System.out.println("Zipcode to change: ");
+                Zipcode = in.next();
+                person.setZipcode(Zipcode);
+                break;
+            case "8":
+                System.out.println("Phone number to change: ");
+                PhoneNumber = in.next();
+                person.setPhoneNumber(PhoneNumber);
+                break;
+            default:
+                list1.add(person);
+                System.out.println("Enter only 1 to 8 numbers");
+        }
+    }
+
+    public static void main(String[] args) {
+        AddresBook addresBook= new AddresBook();
+        addresBook.DisplayContact1();
+        System.out.println("1");
+        addresBook.Details();
+        System.out.println("2");
+        addresBook.DisplayContact1();
+        System.out.println("3");
+        addresBook.edit();
+        System.out.println("4");
+        addresBook.DisplayContact1();
+    }
+
+}
---
+++ AddressBook/Person.java
@@ -0,0 +1,106 @@
+package com.bridgelabz.AddressBook;
+
+public class Person {
+
+    private String FirstName;
+    private String LastName;
+    private String Gmail;
+    private String Address;
+    private String City;
+    private String State;
+    private String Zipcode;
+    private String PhoneNumber;
+    public Person() {
+
+    }
+    public Person(String firstName, String lastName, String gmail, String address, String city, String state, String zipcode, String phnNumber) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Gmail = gmail;
+        this.Address = address;
+        this.City = city;
+        this.State = state;
+        this.Zipcode = zipcode;
+        this.PhoneNumber = phnNumber;
+    }
+
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getGmail() {
+        return Gmail;
+    }
+
+    public void setGmail(String gmail) {
+        Gmail = gmail;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getZipcode() {
+        return Zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        Zipcode = zipcode;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressBook{" +
+                "FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Gmail='" + Gmail + '\'' +
+                ", Address='" + Address + '\'' +
+                ", City='" + City + '\'' +
+                ", State='" + State + '\'' +
+                ", Zipcode='" + Zipcode + '\'' +
+                ", PhoneNumber='" + PhoneNumber + '\'' +
+                '}';
+    }
+}
+
